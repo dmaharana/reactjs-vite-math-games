8871fe3d10c4f95a4615461b8836458d83980865
Deepak Maharana <dmaharana@gmail.com>
2025-03-09 15:58:13 -0400

    chore: updated icon and cleaned up unsed code

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import MathOperationsGame from './components/MathOperationsGame.jsx'
+import MathOperationsGame from "./components/MathOperationsGame.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-	  <MathOperationsGame />
-    </>
-  )
+  return <MathOperationsGame />;
 }
 
-export default App
+export default App;
